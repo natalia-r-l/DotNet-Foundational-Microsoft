dfccd850ffa89bf761f770f9923403623e8bb45f
Natália Resina <resina.natalia@gmail.com>
2024-01-23 15:55:17 -0300

    math operations module

---
+++ Course1/BasicOperationsOnNumbers/ImplicitConversion.cs
@@ -0,0 +1,24 @@
+
+/*
+// Declaração e inicialização da variável firstName
+string firstName = "Bob";
+
+// Declaração e inicialização da variável widgetsSold
+int widgetsSold = 7;
+
+// Concatenação de caracteres literais com variáveis. O compilador interpreta que o número 7 deve ser colocado imediatamente
+// ao lado do valor da variável widgetsSold, exibindo 77. 
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+
+
+// Declaração e inicialização da variável firstName
+string secondName = "Bobi";
+
+// Declaração e inicialização da variável widgetsSold
+int widgetsSoldTwo = 7;
+
+// Concatenação de caracteres literais com varáveis, e definição de ordem de operações por meio do operador ().
+// Neste caso, o compilador entende que a variável widgetsSold deve ser somada ao número 7, e o resultado exibido é 14.
+Console.WriteLine(secondName + " sold " + (widgetsSoldTwo + 7) + " widgets.");
+
+*/
---
+++ Course1/BasicOperationsOnNumbers/MathOperations.cs
@@ -0,0 +1,37 @@
+/*
+
+// Operação de soma;
+int sum = 7 + 5;
+Console.WriteLine("Sum: " + sum);
+
+// Operação de subtração
+int difference = 7 - 5;
+Console.WriteLine("Difference: " + difference);
+
+// Operação de Multiplicação 
+int product = 7 * 5;
+Console.WriteLine("Product: " + product);
+
+// Operação de Divisão
+int quotient = 7 / 5;
+Console.WriteLine("Quotient: " + quotient);
+
+// Operações com decimais
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+// Declaração da variável first como tipo int. 
+int first = 7;
+
+// Declaração da variável second como tipo int. 
+int second = 5;
+
+// Declaração da variável quotient, do tipo decimal, e conversão das variáveis first e second, do tipo int, para o tipo decimal.
+decimal quotient = (decimal)first / (decimal)second;
+Console.WriteLine(quotient);
+
+// Cálculo do resto da divisão, conhecido como módulo, e aplicado por meio do operador %. 
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+*/

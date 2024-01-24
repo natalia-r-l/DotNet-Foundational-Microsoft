f13f7c8cdb3702759675225d3af175008d15a547
Natália Resina <resina.natalia@gmail.com>
2024-01-24 10:20:09 -0300

    conditional module

---
+++ CourseTwo/BooleanExpression.cs
@@ -0,0 +1,12 @@
+/*
+
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
+
+*/
---
+++ CourseTwo/MethodsWithInstance.cs
@@ -0,0 +1,20 @@
+
+/*
+
+// Instanciação da classe Random, para a criação de um objeto dice, por meio do operador new para que possa
+// ser aplicado o método com estado Next(). 
+
+o new funciona solicitando um endereço na memória do computador com tamanho suficiente para armazenar um novo objeto baseado na classe Random.
+Ele cria o objeto e o armazena no endereço de memória.
+E então retorna o endereço de memória para que ele possa ser salvo na variável dice.
+
+Random dice = new Random();
+
+// Declaração e inicialização da variável roll. Utilização do método Next()
+int roll = dice.Next();
+
+// Exibição do valor armazenado em roll;
+Console.WriteLine(roll);
+
+*/
+
